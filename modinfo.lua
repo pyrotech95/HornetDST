45048f1ec56f3f285752de9fc702ba9cbcec3ed3
pyrotech95 <Jamiepheywood@gmail.com>
2022-01-17 20:31:53 +0000

    Added Modinfo.lua File

---
+++ modinfo.lua
@@ -0,0 +1,32 @@
+-- This information tells other players more about the mod
+name = "Hornet"
+description = "A character template for Don't Starve Together."
+author = "Pyrotech95 & The Deadly Cow"
+version = "0.1.0" -- First number is major version, second number minor version. The third number represents bugfix versions
+
+-- This is the URL name of the mod's thread on the forum; the part after the ? and before the first & in the url
+forumthread = "/files/file/950-extended-sample-character/"
+
+-- This lets other players know if your mod is out of date, update it to match the current version in the game
+api_version = 10
+
+-- Compatible with Don't Starve Together
+dst_compatible = true
+
+-- Not compatible with Don't Starve
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+shipwrecked_compatible = false
+
+-- Character mods are required by all clients
+all_clients_require_mod = true 
+
+icon_atlas = "Hornet.xml"
+icon = "Hornet.tex"
+
+-- The mod's tags displayed on the server list
+server_filter_tags = {
+"character",
+}
+
+--configuration_options = {}
